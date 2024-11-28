325899f342b6ab613a0809a59fbb1f42c69a90a8
unknown <dunjoyebiz2020@gmail.com>
2024-11-28 20:44:48 +0100

    adding carousel and media

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,9 @@ import { BlackFriday } from "../utils";
 const Hero = () => {
   return (
     <section className="overflow-hidden gb-mg">
-      <div className="flex flex-col lg:flex-row lg:items-stretch lg:min-h-[700px]">
+      <div className="flex flex-col lg:flex-row lg:items-stretch lg:min-h-[300px]">
         {/* first half of the main hero image */}
-        <div className="relative w-full md:rounded-badge overflow-hidden sm:order-2 lg:order-1 h-96 lg:h-auto lg:w-5/12">
+        <div className="relative w-full md:rounded-badge flex overflow-hidden lg:order-1 h-96 lg:h-auto lg:w-5/12">
           <div className="absolute inset-0">
             <img
               className="object-cover w-full h-full scale-150"
@@ -19,51 +19,35 @@ const Hero = () => {
 
           <div className="absolute bottom-0 left-0">
             <div className="p-4 sm:p-6 lg:p-8">
-              <div className="flex items-center">
-                <svg
-                  className="w-10 h-10 text-orange-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <h2 className="font-bold text-white text-7xl ml-2.5">395</h2>
-              </div>
-              <p className="max-w-xs mt-1.5 text-xl text-white">
-                Professionals have organized their desk via PostCra
+              <div className="flex flex-col items-center">
+            
+                <h2 className="font-bold text-white text-2xl md:text-4xl ml-2.5">Clearance Sales</h2>
+                <p className="max-w-xs mt-1.5 text-xl text-white">
+                Buy one or two from us!
               </p>
+              </div>
+              
             </div>
           </div>
         </div>
 
         {/* second half of the main hero image */}
-        <div className="relative flex flex-col lg:px-2 sm:order-1 lg:order-2 h-full md:ml-2 bg-black lg:w-7/12">
+        <div className="relative flex flex-col lg:px-2 lg:order-2 h-full md:ml-2 lg:w-7/12">
           {/* First Container */}
-          <div className="flex-1 md:rounded-badge pt-16 md:mb-2 px-4 lg:pt-24 pb-16 lg:py-16 bg-red-500 flex-col text-center items-center justify-center">
+          <div className="flex-1 md:rounded-badge pt-16 md:my-2 px-4 lg:pt-24 pb-16 lg:py-16 bg-black flex-col text-center items-center justify-center">
             <h1 className="font-bold text-white ">Big holiday sale</h1>
-            <h2 className="text-white">Up to -50%</h2>
+            <h2 className="text-orange-400 italic">Up to -50%</h2>
           </div>
 
-          {/* Second Container */}
-          <div className="flex-1 rounded-badge bg-blue-500 hidden lg:flex items-center justify-between">
+          
+
+          {/* Second Container hidden on sm*/}
+          <div className="flex-1 rounded-badge hidden md:flex items-center justify-between">
             {/* swirl shii */}
-          <div className="absolute bottom-0 right-0 hidden lg:block z-0">
-            <img
-              className="object-contain w-auto h-48"
-              src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/3/curved-lines.png"
-              alt=""
-            />
-          </div>
-
-            <div className="w-4/12 relative px-1 shadow-lg">
+            <div className="absolute bottom-0 right-0 hidden lg:block z-0">
               <img
-                className="object-cover rounded-box w-full h-full"
-                src={BlackFriday}
+                className="object-contain w-auto h-48"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/3/curved-lines.png"
                 alt=""
               />
             </div>
@@ -83,9 +67,32 @@ const Hero = () => {
                 alt=""
               />
             </div>
+
+            <div className="w-4/12 relative px-1 shadow-lg">
+              <img
+                className="object-cover rounded-box w-full h-full"
+                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+                alt=""
+              />
+            </div>
+
             
           </div>
-          
+
+          {/* hero carousel for sm */}
+          <div className="flex md:hidden align-center justify-center m-3 py-2">
+              <div className="carousel carousel-center rounded-box h-96">
+                <div className="carousel-item h-full">
+                  <img src={BlackFriday} />
+                </div>
+                <div className="carousel-item h-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" />
+                </div>
+                <div className="carousel-item h-full">
+                  <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" />
+                </div>
+              </div>
+            </div>
         </div>
       </div>
     </section>
