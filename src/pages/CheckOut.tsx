16d44b48560f413b171d2bc2279e199ac09b5722
unknown <dunjoyebiz2020@gmail.com>
2025-01-08 18:32:03 +0100

    finished the checkout card

--- src/pages/CheckOut.tsx
+++ src/pages/CheckOut.tsx
@@ -1,25 +1,25 @@
+
 import { useCartContext } from "../context/CartContext";
 import CheckOutCard from "./CheckOutCard";
 
 const CheckOut = () => {
   const [{ cart }] = useCartContext();
 
- 
   //map through the cart to isolate the ids
-  const cartMap = cart.map(item => item.id);
+  const cartMap = cart.map((item) => item.id);
   // select only a single case of each repetitions
   const cartSet = new Set(cartMap);
   // make the cartSet into an array of ids
   const newCartArray = Array.from(cartSet);
 
-  //use the ids to map through the original cart and also do the count 
+  //use the ids to map through the original cart and also do the count
   const uniqueCheckoutList = newCartArray.map((id) => {
     // Find the unique item from the cart
     const uniqueItem = cart.find((item) => item.id === id);
-  
+
     // Count occurrences of this item in the cart
     const count = cart.filter((item) => item.id === id).length;
-  
+
     // Return a new object combining the item and its count
     return {
       ...uniqueItem,
@@ -27,27 +27,23 @@ const CheckOut = () => {
     };
   });
 
-
-  console.log(uniqueCheckoutList);
-
   
- 
-  
+
   return (
     <section>
       <div className="flex flex-col lg:flex-row m-4 gap-4 ">
-        <div className="card lg:w-8/12 bg-red-400 p-2 shadow-xl">
+        <div className="card lg:w-8/12  p-2 shadow-xl">
           <h2 className="card-title">Cart ({cart.length})</h2>
           {uniqueCheckoutList.map((item) => (
             <CheckOutCard
-              key={item.id}
-              imageUrl={item.image}
-              title={item.title}
-              price={item.price}
-              itemCount={item.count}
-              itemId={item.id}
-              
-            />
+            key={item.id}
+            imageUrl={item.image}
+            title={item.title}
+            price={item.price}
+            itemCount={item.count}
+            itemId={item.id}
+           
+          />
           ))}
         </div>
 
